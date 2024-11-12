fec7351250e5c62c1d4f0e1a225bdfec79c7db4e
Abhilove Goyal <140530756+Abhilove-Goyal@users.noreply.github.com>
2024-11-12 17:27:59 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -4,8 +4,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/task_management";
-    private static final String USER = "root";
-    private static final String PASS = "Rohit@123";
+    private static final String USER = "DB_USER";
+    private static final String PASS = "DB_PASS";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(DB_URL, USER, PASS);
